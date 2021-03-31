434d48e6142ee245ce1d8598b71cc104fc3384d4
Saphfy <robbiebowden99@gmail.com>
2021-03-31 14:27:37 +0100

    Updated Chat Fragment xml file.

---
+++ app/src/main/java/com/bowden/robert/friend_finder_app/CustomAdapters/WhosNewAdapter.java
@@ -0,0 +1,58 @@
+package com.bowden.robert.friend_finder_app.CustomAdapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.bowden.robert.friend_finder_app.R;
+
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class WhosNewAdapter extends RecyclerView.Adapter<WhosNewAdapter.WhosNewViewHolder> {
+
+    /*
+    * WhosNewAdapter is used with the chat fragment.
+    */
+
+    private List<Integer> newMatches;
+    private Context context;
+    private int layout;
+
+    public WhosNewAdapter (List<Integer> newMatches, Context context, int layout) {
+        this.newMatches = newMatches;
+        this.context = context;
+        this.layout = layout;
+    }
+
+    @NonNull
+    @Override
+    public WhosNewViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(layout, parent, false);
+        return new WhosNewViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull WhosNewAdapter.WhosNewViewHolder holder, int position) {
+        holder.newMatch.setImageResource(newMatches.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class WhosNewViewHolder extends RecyclerView.ViewHolder {
+
+        private ImageView newMatch;
+
+        public WhosNewViewHolder(@NonNull View itemView) {
+            super(itemView);
+            newMatch = itemView.findViewById(R.id.imageWhosNew);
+        }
+    }
+}
